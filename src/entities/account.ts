639fd930339f419c4d38293a6737329d8b7407d1
sajjadpoores <sajjadpoores@gmail.com>
2023-01-29 00:00:46 +0330

    adding sendmessage for temp in account logic

--- src/entities/account.ts
+++ src/entities/account.ts
@@ -1,33 +1,55 @@
 import { ChatGPTAPIBrowser } from "chatgpt";
 
 export class Account {
-    email: string;
-    password: string;
-    isGoogleLogin: boolean;
-    lastTimeGotToken: Date;
+  email: string;
+  password: string;
+  isGoogleLogin: boolean;
+  lastTimeGotToken: Date;
 
-    async connect() {
-        let api: ChatGPTAPIBrowser;
-        try {
-            api = new ChatGPTAPIBrowser({
-                email: this.email,
-                password: this.password,
-                isGoogleLogin: this.isGoogleLogin,
-            });
-            await api.initSession();
-            this.lastTimeGotToken = new Date();
-            const result = await api.sendMessage("Hello World!");
-
-            console.log(result);
-            api.closeSession();
-            return api;
-        }
-        catch(err) {
-            if(err.statusCode === 429) {
-                console.log("Too many requests");
-            }
-            api.closeSession();
-            return null;
-        }
+  async connect() {
+    let api: ChatGPTAPIBrowser;
+    try {
+      console.log(
+        "Connecting to ChatGPT API for account: " + this.email + " ..."
+      );
+      api = new ChatGPTAPIBrowser({
+        email: this.email,
+        password: this.password,
+        isGoogleLogin: this.isGoogleLogin,
+      });
+      await api.initSession();
+      this.lastTimeGotToken = new Date();
+      
+      return api;
+    } catch (err) {
+      api.closeSession();
+      if (err.statusCode === 429) {
+        // TODO: handle too many requests by waiting 5 seconds
+        console.log("Too many requests");
+        return null;
+      } else if (
+        err.message.includes(
+          "Navigation failed because browser has disconnected!"
+        ) ||
+        err.message.includes(
+          "Protocol error (Runtime.callFunctionOn): Target closed."
+        )
+      ) {
+        console.log("Browser has disconnected");
+        // TODO: Handle this error by restarting the browser
+        return null;
+      }
+      console.log(err.message);
     }
+  }
+
+  async sendMssg(api: ChatGPTAPIBrowser, mssg: string) {
+    try {
+      await api.sendMessage(mssg);
+      return true;
+    } catch (err) {
+      console.log(err.message);
+      return false;
+    }
+  }
 }
